f4dc0582c6fb54cd5c36a27a4e1405767481279c
Arkady Pevzner <arkady.pevzner@gmail.com>
2014-01-04 12:50:49 -0800

    Filled in 'memoize' function

--- src/underbar.js
+++ src/underbar.js
@@ -260,6 +260,14 @@ var _ = { };
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var results = {};
+
+    return function(key) {
+      if(!(key in results)) {
+        results[key] = func.call(this, key);
+      }
+      return results[key];
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
