663585f240cb23beda4afb4db74119ad62820ba7
Arkady Pevzner <arkady.pevzner@gmail.com>
2014-01-04 12:31:41 -0800

    Filled in 'some' function

--- src/underbar.js
+++ src/underbar.js
@@ -175,8 +175,12 @@ var _ = { };
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
-  };
+    var _iterator = (iterator === undefined) ? function(val){return Boolean(val)} : iterator;
 
+    return !_.every(collection, function(val) {
+      return !_iterator(val); 
+    })
+  };
 
   /**
    * OBJECTS
