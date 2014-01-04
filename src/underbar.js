544ae354c829258f1617ef9c4c869a9f4f578958
Arkady Pevzner <arkady.pevzner@gmail.com>
2014-01-03 19:46:45 -0800

    Filled in 'invoke' function

--- src/underbar.js
+++ src/underbar.js
@@ -120,6 +120,9 @@ var _ = { };
 
   // Calls the method named by methodName on each value in the list.
   _.invoke = function(list, methodName, args) {
+    return _.map(list, function(value){
+      return (typeof(methodName) === 'function' ? methodName : value[methodName]).apply(value, args);
+    });
   };
 
   // Reduces an array or object to a single value by repetitively calling
